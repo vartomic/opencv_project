4c1e1d25688d5b46748524ab17937d75f156ad66
vartomic <varrava16@mail.ru>
2023-09-12 22:27:25 +0300

    12/09/23

--- opencv_project/source/model/hsv_model.cpp
+++ opencv_project/source/model/hsv_model.cpp
@@ -14,19 +14,29 @@ cv::Mat HSVmodel::process(cv::Mat frame) {
 
 	cv::cvtColor(frame, hsvImage, cv::COLOR_BGR2HSV);
 
+	//par1 = 165, par2 = 42
+
 	//trackbar1 from 0 to 240, pos = 100, range 75-125
 	int hue_from = _par1 - 25;
 	int hue_to = _par1 + 25;
 
-
-	if (hue_from < 0 || hue_to > 240){
+	if (hue_from < 0){
 		hue_from = 0;
-		hue_to > 240;
+	}
+	if (hue_to > 240) {
+		hue_to = 240;
 	}
 
 	//trackbar2 from 0 to 240, pos = 240
 	int sat_from = _par2 - 25;
 	int sat_to = _par2 + 25;
+
+	if (sat_from < 0) {
+		sat_from = 0;
+	}
+	if (sat_to > 240) {
+		sat_to = 240;
+	}
 
 	//brightness
 	int val_from = 30;
@@ -41,21 +51,28 @@ cv::Mat HSVmodel::process(cv::Mat frame) {
 		cv::Scalar(hue_to, sat_to, val_to), rangeImage);
 
 	//третий трекбар
+	//TickMeter
 
 	cv::Mat dilateImage; 
-	cv::dilate(rangeImage, dilateImage, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(3, 3)));
+	cv::dilate(rangeImage, dilateImage, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(50, 50)));
 
 	//vector of vectors of points
 	std::vector<std::vector<cv::Point>> contours;
+	std::vector<cv::Vec4i> hierarchy;
+	std::vector<cv::Point> approx;
 
 	// finds outer contours, using only 4 corner points
 	cv::findContours(dilateImage, contours, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
 
-	for (auto contour : contours) {
+	cv::Scalar green(0, 255, 0);
 
-		cv::Scalar color(0, 255, 0);
-		cv::drawContours(frame, contours, -1, color, cv::FILLED, 8);
+	cv::Mat resImage;
+
+	for (size_t i = 0; i < contours.size(); i++) {
+		cv::drawContours(frame, contours, i, green, 8, 8, hierarchy);
+		//cv::approxPolyDP(contours[i], approx, 10, true);
+		auto rect = cv::boundingRect(contours[i]);
+		cv::rectangle(frame, rect, cv::Scalar(0, 255, 0));
 	}
-
-	return dilateImage;
+	return frame;
 }
