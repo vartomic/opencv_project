b087e9bdb4a1262b65ddebc851477f9c0d20ed2f
vartomic <varrava16@mail.ru>
2023-11-11 17:04:59 +0300

    11/11/23

--- opencv_project/source/data/data_photo.cpp
+++ opencv_project/source/data/data_photo.cpp
@@ -6,15 +6,16 @@ namespace fs = std::filesystem;
 
 cv::Mat DataPhoto::getData() {
 	return _img;
-}
 void DataPhoto::nextImage() {
 	//	Path to images
 	std::string imgPath = "img/faces/";
-
+	//	Vector for images
 	std::vector<cv::String> VEC_IMAGES;
-	
+	//	fs::path represents paths on a filesystem
+	//	directory_iterator iterates over the elements of a directory (but does not visit the subdirectories)
 	for (const fs::path& p : fs::directory_iterator(imgPath)) {
-		VEC_IMAGES.push_back(p.u8string());
+		//	File paths are being converted in string and placed in a vector
+		VEC_IMAGES.push_back(p.string());
 	}
 	//	Reads img
 	_img = cv::imread(VEC_IMAGES[_curImageIndex]);
@@ -24,7 +25,6 @@ void DataPhoto::nextImage() {
 	}
 	//	Index increases on 1
 	_curImageIndex++;
-
 	//	If current index of array greater than size of array current index equalizes to 0
 	if (_curImageIndex >= VEC_IMAGES.size()) {
 		_curImageIndex = 0;
