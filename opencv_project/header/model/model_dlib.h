649438ab6c8a7ef32d891510764d05eb2a14152c
vartomic <varrava16@mail.ru>
2023-11-18 14:37:11 +0300

    18/11/23

--- opencv_project/header/model/model_dlib.h
+++ opencv_project/header/model/model_dlib.h
@@ -7,13 +7,16 @@ class DlibModel {
 public:
 	// Load face detection and pose estimation models.
 	dlib::frontal_face_detector detector;
+	//
+	std::vector<dlib::rectangle> faces;
 
 	DlibModel() {
 		//
 		detector = dlib::get_frontal_face_detector();
 	}
 
-	void visualize(cv::Mat& image);
+	void visualize(cv::Mat& image, int frame, std::vector<dlib::rectangle>& faces, double fps, int thickness = 2);
 
 	cv::Mat process(cv::Mat frame);
+
 };
--- opencv_project/source/model/model_dlib.cpp
+++ opencv_project/source/model/model_dlib.cpp
@@ -1,6 +1,6 @@
 #include "model/model_dlib.h"
 
-void DlibModel::visualize(cv::Mat& image) {
+void DlibModel::visualize(cv::Mat& image, int frame, std::vector<dlib::rectangle>& faces, double fps, int thickness) {
     //  Text string with fps value formatted from double to float
     std::string fpsString = cv::format("FPS : %.2f", (float)fps);
     //  Receives the width of input image
@@ -9,16 +9,13 @@ void DlibModel::visualize(cv::Mat& image) {
     int height = image.size().height;
     //  Text string with width and height of input image
     std::string resolutionString = cv::format("Resolution : %d x %d", width, height);
-	// Detect faces 
-	std::vector<dlib::rectangle> faces = detector(image);
-
-	std::vector<dlib::full_object_detection> shapes;
-	for (unsigned long i = 0; i < faces.size(); ++i){
-		//shapes.push_back(image, faces[i]));//
-    }
+    //  Function draws the text string with fps value in the image
+    putText(image, fpsString, cv::Point(0, 15), cv::FONT_ITALIC, 0.5, cv::Scalar(0, 255, 255), thickness);
+    //  Function draws the text string with resolution of input image on the image
+    putText(image, resolutionString, cv::Point(0, 35), cv::FONT_ITALIC, 0.5, cv::Scalar(0, 0, 255), thickness);
 }
 
-cv::Mat process(cv::Mat frame) {
+cv::Mat DlibModel::process(cv::Mat frame) {
     //  Checks if frame is empty
     if (frame.empty()) {
         return cv::Mat();
@@ -29,10 +26,12 @@ cv::Mat process(cv::Mat frame) {
     tm.start();
     //  Creates a clone of an input image
     cv::Mat image = frame.clone();
-    
+    // Detect faces 
+    faces = detector(image);
     //  Tickmeter stops
     tm.stop();
-
+    //
+    visualize(image, -1, faces, tm.getFPS());
     //  Returns processed image
     return image;
 
